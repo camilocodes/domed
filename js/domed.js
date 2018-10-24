b6f406be5faacab0a64580dfac77a5266403f004
camilocodes <camilo.v@unb.ca>
2018-10-24 16:21:09 -0300

    Getting graphics onscreen and staying OO

--- js/domed.js
+++ js/domed.js
@@ -5,20 +5,32 @@ by Camilo Villamizar
 est. 2018
 */
 
-/* Sprite Class */
+// Sprite Class
 class DSprite {
 
-	constructor(parentElement) {
-		this.parent = parentElement;
-		// Create DOM sprite.
-		this.element = document.createElement("div");
+	constructor() {
+		// Create and configure DOM sprite container.
+		this.element = document.createElement('div');
 		this.element.className = 'sprite';
 		// Optimized pointer to style.
 		this.style = this.element.style;
+
+		// Load sprite sheet.
+		this.loadSheet = function(imageLocation) {
+			this.style.backgroundImage = `url(${imageLocation})`;
+		};
+
+		// Set sprite dimensions (in pixels).
+		this.setDimensions = function(width, height) {
+			this.style.width =  `${width}px`;
+			this.style.height =  `${height}px`;
+		};
 	}
 }
 
 window.onload = function() {
-	let mySprite = new DSprite(document.getElementById('viewport'));
-	alert(mySprite.parent);
+	let mySprite = new DSprite();
+	mySprite.loadSheet('./gfx/game/ball.png');
+	mySprite.setDimensions(8, 8);
+	document.getElementById('viewport').appendChild(mySprite.element);
 };
